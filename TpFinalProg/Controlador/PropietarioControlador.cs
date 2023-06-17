131d6f5c906cf30ede0a69142fdf3f1f183611ff
Lucas Ferrero <lucasdavidferrero@gmail.com>
2023-06-17 15:32:08 -0300

    Agrego primer controller para hacer el primer ejemplo de ABM.

---
+++ TpFinalProg/Controlador/PropietarioControlador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpFinalProg.Controlador {
+    internal class PropietarioControlador {
+
+        public static void crear () {
+
+        }
+
+        public static void actualizar () { }
+
+        public static void eliminar() { }
+
+        public static void listar() { }
+
+    }
+}
