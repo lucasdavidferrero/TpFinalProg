6b2f995f6c11ef2f1fb2459680a59845916c9972
Lucas Ferrero <lucasdavidferrero@gmail.com>
2023-06-17 11:48:25 -0300

    agrego editor config

---
+++ TpFinalProg/Entidades/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpFinalProg.Entidades {
+    internal class Empleado {
+        private int nroLegajo { get; }
+        private DateOnly fechaIngreso { get; }
+        private string nombre { get; }
+        private string apellido { get; }
+        private string celular { get; }
+        private string email { get; }
+
+        private bool baja { get; }
+
+        public Empleado(int legajo, DateOnly fechaIngreso, string nombre, string apellido, string celular, string email) {
+            this.nroLegajo = legajo;
+            this.fechaIngreso = fechaIngreso;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.celular = celular;
+            this.email = email;
+        }
+    }
+}
