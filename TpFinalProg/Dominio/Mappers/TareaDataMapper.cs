99731a7fbc088d8a7e6ccf1710237b101f6e6185
Mateovalenzuela <mateo.valenzuela.dev@gmail.com>
2023-07-01 03:35:07 -0300

    Agragdo de mappers funcionando

---
+++ TpFinalProg/Dominio/Mappers/TareaDataMapper.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TpFinalProg.Clases;
+using TpFinalProg.Dominio.Entidades;
+
+namespace PruebaTpFinal.Dominio.Mappers
+{
+    internal class TareaDataMapper
+    {
+        public static Tuple<int, int> InsertNew(Tarea tarea)
+        {
+            string query = @"
+        INSERT INTO Tarea (id_proyecto, nro_tarea, descripcion, horas_estimadas, horas_avance, costo_estimado, horas_reales, costo_real, fecha_final)
+        OUTPUT INSERTED.id_proyecto, INSERTED.nro_tarea
+        VALUES (@idProyecto, @idTarea, @descripcion, @horasEstimadas, @horasAvance, @costoEstimado, @horasReales, @costoReal, @fechaFinal)";
+
+            Tuple<int, int> generatedId = null;
+            Conexion cx = new Conexion();
+            SqlCommand cmd = cx.getComando();
+
+            cmd.Parameters.Add("@idProyecto", SqlDbType.Int);
+            cmd.Parameters.Add("@idTarea", SqlDbType.Int);
+            cmd.Parameters.Add("@descripcion", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@horasEstimadas", SqlDbType.Int);
+            cmd.Parameters.Add("@horasAvance", SqlDbType.Int);
+            cmd.Parameters.Add("@costoEstimado", SqlDbType.Float);
+            cmd.Parameters.Add("@horasReales", SqlDbType.Int);
+            cmd.Parameters.Add("@costoReal", SqlDbType.Float);
+            cmd.Parameters.Add("@fechaFinal", SqlDbType.Date);
+
+            cmd.Parameters["@idProyecto"].Value = tarea.idProyecto;
+            cmd.Parameters["@idTarea"].Value = tarea.idTarea;
+            cmd.Parameters["@descripcion"].Value = tarea.descripcion;
+            cmd.Parameters["@horasEstimadas"].Value = tarea.horasEstimadas;
+            cmd.Parameters["@horasAvance"].Value = tarea.horasAvance;
+            cmd.Parameters["@costoEstimado"].Value = tarea.costoEstimado;
+            cmd.Parameters["@horasReales"].Value = tarea.horasReales;
+            cmd.Parameters["@costoReal"].Value = tarea.costoReal;
+            cmd.Parameters["@fechaFinal"].Value = tarea.fechaFinal;
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        int idProyecto = Convert.ToInt32(reader["id_proyecto"]);
+                        int idTarea = Convert.ToInt32(reader["nro_tarea"]);
+                        generatedId = Tuple.Create(idProyecto, idTarea);
+                    }
+
+                    return generatedId;
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Insertar Tarea]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+            return generatedId;
+        }
+
+        public static List<Tarea> GetAll()
+        {
+            List<Tarea> tareas = new List<Tarea>();
+            string query = "SELECT * FROM Tarea";
+            Conexion cx = new Conexion();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                SqlDataAdapter sqlDat = new SqlDataAdapter(cx.getComando());
+                sqlDat.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    int idProyecto = Convert.ToInt32(row["id_proyecto"]);
+                    int idTarea = Convert.ToInt32(row["nro_tarea"]);
+                    string descripcion = row["descripcion"].ToString();
+                    int horasEstimadas = Convert.ToInt32(row["horas_estimadas"]);
+                    int horasAvance = Convert.ToInt32(row["horas_avance"]);
+                    float costoEstimado = Convert.ToSingle(row["costo_estimado"]);
+                    int horasReales = Convert.ToInt32(row["horas_reales"]);
+                    float costoReal = Convert.ToSingle(row["costo_real"]);
+                    DateTime fechaFinal = (DateTime)row["fecha_final"];
+
+                    Tarea tarea = new Tarea(idProyecto, idTarea, descripcion, horasEstimadas, horasAvance, costoEstimado, horasReales, costoReal, fechaFinal);
+                    tareas.Add(tarea);
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Listado Tareas]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+
+            return tareas;
+        }
+
+        public static Tuple<int, int> Update(Tarea tarea)
+        {
+            string query = "UPDATE Tarea SET descripcion = @descripcion, horas_estimadas = @horasEstimadas, horas_avance = @horasAvance, " +
+                           "costo_estimado = @costoEstimado, horas_reales = @horasReales, costo_real = @costoReal, fecha_final = @fechaFinal " +
+                           "WHERE id_proyecto = @idProyecto AND nro_tarea = @idTarea";
+
+            Conexion cx = new Conexion();
+            SqlCommand cmd = cx.getComando();
+
+            cmd.Parameters.Add("@descripcion", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@horasEstimadas", SqlDbType.Int);
+            cmd.Parameters.Add("@horasAvance", SqlDbType.Int);
+            cmd.Parameters.Add("@costoEstimado", SqlDbType.Float);
+            cmd.Parameters.Add("@horasReales", SqlDbType.Int);
+            cmd.Parameters.Add("@costoReal", SqlDbType.Float);
+            cmd.Parameters.Add("@fechaFinal", SqlDbType.Date);
+            cmd.Parameters.Add("@idProyecto", SqlDbType.Int);
+            cmd.Parameters.Add("@idTarea", SqlDbType.Int);
+
+            cmd.Parameters["@descripcion"].Value = tarea.descripcion;
+            cmd.Parameters["@horasEstimadas"].Value = tarea.horasEstimadas;
+            cmd.Parameters["@horasAvance"].Value = tarea.horasAvance;
+            cmd.Parameters["@costoEstimado"].Value = tarea.costoEstimado;
+            cmd.Parameters["@horasReales"].Value = tarea.horasReales;
+            cmd.Parameters["@costoReal"].Value = tarea.costoReal;
+            cmd.Parameters["@fechaFinal"].Value = tarea.fechaFinal;
+            cmd.Parameters["@idProyecto"].Value = tarea.idProyecto;
+            cmd.Parameters["@idTarea"].Value = tarea.idTarea;
+
+            Tuple<int, int> idEncontado = null;
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        int idProyecto = Convert.ToInt32(reader["id_proyecto"]);
+                        int idTarea = Convert.ToInt32(reader["nro_tarea"]);
+                        idEncontado = Tuple.Create(idProyecto, idTarea);
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Actualizar Tarea]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+            return idEncontado;
+        }
+
+        public static Tarea FindById(int idProyecto, int idTarea)
+        {
+            Tarea tareaEncontrada = null;
+            string query = "SELECT * FROM Tarea WHERE id_proyecto = @idProyecto AND nro_tarea = @idTarea";
+            DataTable dt = new DataTable();
+            Conexion cx = new Conexion();
+            SqlCommand cmd = cx.getComando();
+
+            cmd.Parameters.AddWithValue("@idProyecto", idProyecto);
+            cmd.Parameters.AddWithValue("@idTarea", idTarea);
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                SqlDataAdapter sqlDat = new SqlDataAdapter(cx.getComando());
+                sqlDat.Fill(dt);
+
+                if (dt.Rows.Count != 0)
+                {
+                    DataRow row = dt.Rows[0];
+                    string descripcion = row["descripcion"].ToString();
+                    int horasEstimadas = Convert.ToInt32(row["horas_estimadas"]);
+                    int horasAvance = Convert.ToInt32(row["horas_avance"]);
+                    float costoEstimado = Convert.ToSingle(row["costo_estimado"]);
+                    int horasReales = Convert.ToInt32(row["horas_reales"]);
+                    float costoReal = Convert.ToSingle(row["costo_real"]);
+                    DateTime fechaFinal = (DateTime)row["fecha_final"];
+
+                    tareaEncontrada = new Tarea(idProyecto, idTarea, descripcion, horasEstimadas, horasAvance, costoEstimado, horasReales, costoReal, fechaFinal);
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Obtener por Id Tarea]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+
+            return tareaEncontrada;
+        }
+    }
+
+}
---
+++ TpFinalProg/Dominio/Mappers/TrabajaDataMapper.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TpFinalProg.Clases;
+using TpFinalProg.Dominio.Entidades;
+using static System.Windows.Forms.MonthCalendar;
+
+namespace PruebaTpFinal.Dominio.Mappers
+{
+    internal class TrabajaDataMapper
+    {
+        public static Tuple<int, int, int> InsertNew(Trabaja trabaja)
+        {
+            string query = @"INSERT INTO Trabaja(legajo, id_proyecto, id_tarea, id_funcion_fk)
+                OUTPUT inserted.legajo, inserted.id_proyecto, inserted.id_tarea
+                VALUES (@nroLegajo, @idProyecto, @idTarea, @idFuncion)";
+
+
+            Tuple<int, int, int> generatedId = null;
+            Conexion cx = new Conexion();
+            SqlCommand cmd = cx.getComando();
+
+            cmd.Parameters.Add("@nroLegajo", SqlDbType.Int);
+            cmd.Parameters.Add("@idProyecto", SqlDbType.Int);
+            cmd.Parameters.Add("@idTarea", SqlDbType.Int);
+            cmd.Parameters.Add("@idFuncion", SqlDbType.Int);
+
+            cmd.Parameters["@nroLegajo"].Value = trabaja.nroLegajo;
+            cmd.Parameters["@idProyecto"].Value = trabaja.idProyecto;
+            cmd.Parameters["@idTarea"].Value = trabaja.idTarea;
+            cmd.Parameters["@idFuncion"].Value = trabaja.idFuncion;
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        int legajo = Convert.ToInt32(reader["legajo"]);
+                        int idProyecto = Convert.ToInt32(reader["id_proyecto"]);
+                        int idTarea = Convert.ToInt32(reader["id_tarea"]);
+                        generatedId = Tuple.Create(legajo, idProyecto, idTarea);
+                    }
+
+                    return generatedId;
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Insertar Trabaja]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+            return generatedId;
+        }
+
+        public static List<Trabaja> GetAll()
+        {
+            List<Trabaja> trabajas = new List<Trabaja>();
+            string query = "SELECT * FROM Trabaja";
+            Conexion cx = new Conexion();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                SqlDataAdapter sqlDat = new SqlDataAdapter(cx.getComando());
+                sqlDat.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    int nroLegajo = Convert.ToInt32(row["legajo"]);
+                    int idProyecto = Convert.ToInt32(row["id_proyecto"]);
+                    int idTarea = Convert.ToInt32(row["id_tarea"]);
+                    int idFuncion = Convert.ToInt32(row["id_funcion_fk"]);
+
+                    Trabaja trabaja = new Trabaja(nroLegajo, idProyecto, idTarea, idFuncion);
+                    trabajas.Add(trabaja);
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Listado Trabajas]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+
+            return trabajas;
+        }
+
+        public static Tuple<int, int, int> Update(Trabaja trabaja)
+        {
+            string query = "UPDATE Trabaja SET id_funcion_fk = @idFuncion WHERE legajo = @nroLegajo AND id_proyecto = @idProyecto AND id_tarea = @idTarea";
+            Conexion cx = new Conexion();
+            SqlCommand cmd = cx.getComando();
+
+            cmd.Parameters.Add("@idFuncion", SqlDbType.Int);
+            cmd.Parameters.Add("@nroLegajo", SqlDbType.Int);
+            cmd.Parameters.Add("@idProyecto", SqlDbType.Int);
+            cmd.Parameters.Add("@idTarea", SqlDbType.Int);
+
+            cmd.Parameters["@idFuncion"].Value = trabaja.idFuncion;
+            cmd.Parameters["@nroLegajo"].Value = trabaja.nroLegajo;
+            cmd.Parameters["@idProyecto"].Value = trabaja.idProyecto;
+            cmd.Parameters["@idTarea"].Value = trabaja.idTarea;
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                cmd.ExecuteScalar();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Actualizar Trabaja]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+            return Tuple.Create(trabaja.nroLegajo, trabaja.idProyecto, trabaja.idTarea);
+        }
+
+        public static Trabaja FindById(int nroLegajo, int idProyecto, int idTarea)
+        {
+            Trabaja trabajaEncontrada = null;
+            string query = "SELECT * FROM Trabaja WHERE legajo = @nroLegajo AND id_proyecto = @idProyecto AND id_tarea = @idTarea";
+            DataTable dt = new DataTable();
+            Conexion cx = new Conexion();
+            SqlCommand cmd = cx.getComando();
+
+            cmd.Parameters.AddWithValue("@nroLegajo", nroLegajo);
+            cmd.Parameters.AddWithValue("@idProyecto", idProyecto);
+            cmd.Parameters.AddWithValue("@idTarea", idTarea);
+
+            try
+            {
+                cx.SetComandoSQL(query);
+                SqlDataAdapter sqlDat = new SqlDataAdapter(cx.getComando());
+                sqlDat.Fill(dt);
+
+                if (dt.Rows.Count != 0)
+                {
+                    DataRow row = dt.Rows[0];
+                    int idFuncion = Convert.ToInt32(row["id_funcion_fk"]);
+
+                    trabajaEncontrada = new Trabaja(nroLegajo, idProyecto, idTarea, idFuncion);
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error en la base de datos. [Obtener por Id Trabaja]");
+            }
+            finally
+            {
+                cx.cerrarConexionLiberarRecursos();
+            }
+
+            return trabajaEncontrada;
+        }
+    }
+
+}
