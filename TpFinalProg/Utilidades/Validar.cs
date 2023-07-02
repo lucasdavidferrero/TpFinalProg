8bfb29578fb7d3e6420c06286c0bfee3d9cd99b8
Mateovalenzuela <mateo.valenzuela.dev@gmail.com>
2023-07-02 16:25:52 -0300

    Agregado de validar cuit

--- TpFinalProg/Utilidades/Validar.cs
+++ TpFinalProg/Utilidades/Validar.cs
@@ -30,5 +30,33 @@ namespace TpFinalProg.Utilidades {
                 return false;
             }
         }
+
+
+        private static int calcularDigitoCuit(string cuit) {
+            int[] mult = new[] { 5, 4, 3, 2, 7, 6, 5, 4, 3, 2 };
+            char[] nums = cuit.ToCharArray();
+            int total = 0;
+
+            for (int i = 0; i < mult.Length; i++) {
+                total += int.Parse(nums[i].ToString()) * mult[i];
+            }
+
+            var resto = total % 11;
+            return resto == 0 ? 0 : resto == 1 ? 9 : 11 - resto;
+        }
+
+        public static bool cuit(string cuit) {
+            if (cuit == null) {
+                return false;
+            }
+
+            if (cuit.Length != 11) {
+                return false;
+            } else {
+                int calculado = calcularDigitoCuit(cuit);
+                int digito = int.Parse(cuit.Substring(10));
+                return calculado == digito;
+            }
+        }
     }
 }
