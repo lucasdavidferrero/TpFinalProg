03c796dac2422517efe2cd1ec1473a3340221cb5
Lucas Ferrero <lucasdavidferrero@gmail.com>
2023-07-03 20:31:55 -0300

    limpio un toque el codigo

--- TpFinalProg/Formularios/ProyectoFrm.Designer.cs
+++ TpFinalProg/Formularios/ProyectoFrm.Designer.cs
@@ -216,7 +216,7 @@
             // 
             // RESPONSABLE
             // 
-            RESPONSABLE.DataPropertyName = "column1";
+            RESPONSABLE.DataPropertyName = "Column1";
             RESPONSABLE.Frozen = true;
             RESPONSABLE.HeaderText = "Responsable";
             RESPONSABLE.Name = "RESPONSABLE";
